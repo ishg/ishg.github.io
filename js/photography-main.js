738ead123eb8965c8d4be0358c33dc50debea15b
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2016-07-25 21:17:39 +0100

    Try fixing viewer positioning

--- js/photography-main.js
+++ js/photography-main.js
@@ -440,7 +440,8 @@ var main = (function($) { var _ = {
  							// Set background stuff.
 	 							s.$slideImage
 		 							.css('background-image', '')
-		 							.css('background-position', ($thumbnail.data('position') || 'center'));
+		 							.css('background-positon', 'center');
+		 							//.css('background-position', ($thumbnail.data('position') || 'center'));
 
 						// Caption.
 							s.$slideCaption = s.$slide.find('.caption');
